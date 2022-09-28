321f1976a4b0e43e25b7fbff360b2a780e02a1ba
urvashi-19 <0308urvashi@gmail.com>
2022-09-28 16:58:36 +0800

    hi

---
+++ static/new.js
@@ -0,0 +1,50 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAsmwiVI7G26g03tK8W4K4IfSpKUX1Pin0",
+    authDomain: "contactform1-3efca.firebaseapp.com",
+    databaseURL: "https://contactform1-3efca-default-rtdb.firebaseio.com",
+    projectId: "contactform1-3efca",
+    storageBucket: "contactform1-3efca.appspot.com",
+    messagingSenderId: "509156117955",
+    appId: "1:509156117955:web:e26e8cf54fb6b38cd24176",
+    measurementId: "G-0811575EWS"
+  };
+
+
+  firebase.initializeApp(firebaseConfig);
+
+  //
+var contactFormDB1 = firebase.database().ref('contactForm1');
+
+document.getElementById('login').addEventListener('submit',submitForm);
+
+function submitForm(e){
+    e.preventDefault();
+  var  name1 = getElementVal('name1');
+
+  var email1 = getElementVal('email1');
+  var msg = getElementVal('msg');
+  
+
+  saveMessages(name1 , email1,msg);
+
+  console.log(name1 , email1 , msg);
+
+  document.querySelector('.alert').style.display = 'block';
+}
+
+const saveMessages = (name1 , email1,msg)=>{
+    var newContactForm = contactFormDB1.push();
+
+    newContactForm.set({
+        name1:name1,
+        email1 : email1,
+        msg :msg
+
+    })
+};
+
+
+const getElementVal = (id) => {
+    return document.getElementById(id).value;
+  };
+
